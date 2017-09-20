b5f6687d74bf17cc8c204990aa4ea7b7d4444281
Lars Trieloff <trieloff@adobe.com>
2017-09-20 15:06:47 +0200

    #38 find schema to validate against

--- validate.sh
+++ validate.sh
@@ -3,7 +3,7 @@
 examples=$(find schemas -name "*.example.*.json")
 schemas=$(find schemas -name "*.schema.json")
 for example in $examples; do
-  echo $example
-  echo $example | sed -e "s/\.example\..\./.schema./"
+  schema=$(echo $example | sed -e "s/\.example\..\./.schema./")
+  echo "Validating $example against $schema"
   exit 1
 done

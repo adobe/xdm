6265c4856fd4ce6be0d325bf0d5adc6b4d7a77fe
Lars Trieloff <trieloff@adobe.com>
2017-09-20 15:16:13 +0200

    #39 validate with ajv

--- validate.sh
+++ validate.sh
@@ -2,8 +2,19 @@
 
 examples=$(find schemas -name "*.example.*.json")
 schemas=$(find schemas -name "*.schema.json")
+failures=0
+
 for example in $examples; do
   schema=$(echo $example | sed -e "s/\.example\..\./.schema./")
   echo "Validating $example against $schema"
-  exit 1
+  ./node_modules/.bin/ajv validate -s $schema -d $example
+  if [ $? -ne 0 ]; then
+    failures=$[failures + 1]
+  fi
 done
+
+if [ $failures -ne 0 ]; then
+  echo $failures schemas failed to validate
+fi
+
+exit $failures

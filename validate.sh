000871b4ea75614f62ee9a504811815ce49a8cef
Lars Trieloff <trieloff@adobe.com>
2017-09-20 15:36:15 +0200

    #38 load all schemas to validate

--- validate.sh
+++ validate.sh
@@ -2,12 +2,14 @@
 
 examples=$(find schemas -name "*.example.*.json")
 schemas=$(find schemas -name "*.schema.json")
+append="-r $(echo $schemas | sed -e "s/ / -r /g")"
 failures=0
 
 for example in $examples; do
   schema=$(echo $example | sed -e "s/\.example\..\./.schema./")
   echo "Validating $example against $schema"
-  ./node_modules/.bin/ajv validate -s $schema -d $example
+  ./node_modules/.bin/ajv validate -s $schema -d $example $append
+  exit 1
   if [ $? -ne 0 ]; then
     failures=$[failures + 1]
   fi

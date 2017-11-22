47ceca810972a909951cf0832f87eee2e6ffefc0
Lars Trieloff <trieloff@adobe.com>
2017-11-22 06:56:53 +0100

    [trivial] first stab at introducing false positive checking
    Validates all *.invalid.*.json files to make sure they actually get
    marked as invalid.

--- validate.sh
+++ validate.sh
@@ -2,6 +2,8 @@
 
 examples=$(find schemas -name "*.example.*.json")
 schemas=$(find schemas -name "*.schema.json")
+invalid=$(find schemas -name "*.invalid.*.json")
+
 append="-r $(echo $schemas | sed -e "s/ / -r /g")"
 failures=0
 
@@ -17,6 +19,23 @@ done
 
 if [ $failures -ne 0 ]; then
   echo $failures schemas failed to validate
+fi
+
+falsepositives=0
+
+for example in $invalid; do
+  schema=$(echo $example | sed -e "s/\.invalid\..\./.schema./")
+  echo "Validating $example against $schema"
+  ./node_modules/.bin/ajv validate --errors=text --all-errors -s $schema -d $example $append
+
+  if [ $? -e 0 ]; then
+    falsepositives=$[falsepositives + 1]
+  fi
+done
+
+
+if [ $falsepositives -ne 0 ]; then
+  echo $falsepositives schemas failed to validate
 fi
 
 exit $failures

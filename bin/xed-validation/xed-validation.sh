add1dc9a0d051927a4d8650b9dd86ce8b13aba1a
jwen <jwen@adobe.com>
2019-04-16 15:15:19 -0700

    Add validation of schemas

---
+++ bin/xed-validation/xed-validation.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+(rm -rf tempinput; mkdir tempinput)
+cp -r ../../schemas ./tempinput/
+cp -r ../../extensions ./tempinput/
+
+(echo "++++++++++Start clean-up for xed conversion of platform schemas only.....++++++++++"; sleep 2)
+sh cleaninput.sh #cleanup non-platform central repo xdms
+
+(echo "++++++++++Start XED conversion pre-processing.....++++++++++"; sleep 2)
+node tempGen.js -i tempinput/schemas -j xdm
+node tempGen.js -i tempinput/extensions -j xdm-extensions
+
+(echo "++++++++++Start XED conversion processing++++++++++....."; sleep 2)
+node xedGen.js -i xdm -j tempxed
+node xedGen.js -i xdm-extensions -j tempxed
+
+(echo "++++++++++Start XED schemas field tagging..... ++++++++++"; sleep 2)
+echo "{}" > tags.json
+node tag4xed.js -i tempxed -j xed
+
+(echo "++++++++++Start XED schema validation..... ++++++++++"; sleep 2)
+sh compile.sh
+
+(rm -rf tempinput xdm xdm-extensions tempxed xed tags.json) #cleanup temp folders

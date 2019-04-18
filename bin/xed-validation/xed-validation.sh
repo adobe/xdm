77ac3372b05e71ff85754524da879b3e2361f89f
jwen <jwen@adobe.com>
2019-04-18 13:04:47 -0700

    Integration with circle

--- bin/xed-validation/xed-validation.sh
+++ bin/xed-validation/xed-validation.sh
@@ -21,5 +21,13 @@ node tag4xed.js -i tempxed -j xed
 
 (echo "++++++++++Start XED schema validation..... ++++++++++"; sleep 2)
 ./compile.sh
+returnCode=$?
 
-(rm -rf tempinput xdm xdm-extensions tempxed xed tags.json) #cleanup temp folders
+if [ $returnCode -ne 0 ]; then
+  echo "There are schema validation errors!"
+  (rm -rf tempinput xdm xdm-extensions tempxed xed tags.json) #cleanup temp folders
+  exit 1
+else
+  echo "All good"
+  (rm -rf tempinput xdm xdm-extensions tempxed xed tags.json) #cleanup temp folders
+fi

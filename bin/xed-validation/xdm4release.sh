429d390265d24fc839a0905889538c2199bafe9c
jwen <jwen@adobe.com>
2019-10-08 12:49:33 -0700

    Add fixes for descriptor validation

--- bin/xed-validation/xdm4release.sh
+++ bin/xed-validation/xdm4release.sh
@@ -2,7 +2,7 @@
 # this script is for registry INT release purpose and should be called only from registry api publish call
 (rm -rf tempinput; mkdir tempinput)
 (cp -r ../../schemas ./tempinput/; cp -r ../../extensions ./tempinput/)
-./cleaninput4release.sh #cleanup non-platform central repo xdms
+./cleaninput.sh #cleanup non-platform central repo xdms
 (rm -rf ../../json; mkdir ../../json; mkdir ../../json/release)
 (cp -r ./tempinput/schemas ../../json/release; cp -r ./tempinput/extensions ../../json/release)
 (rm -rf tempinput; mkdir tempinput)

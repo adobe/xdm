ca5314222db64064667d18424885d6a55c95e208
jwen <jwen@adobe.com>
2020-08-21 14:36:24 -0700

    No need for version test anymore

--- test/version.js
+++ test/version.js
@@ -9,7 +9,7 @@ const schemas = $.find("schemas","components").filter(name => {
 const package = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 describe("package.json version", () => {
-  it("config.schemas in package.json is up-to-date", () => {
+  xit("config.schemas in package.json is up-to-date", () => {
     assert.equal(schemas.length, package.config.schemas, "Open package.json and increase the minor version number then update the config.schemas parameter to " +
     schemas.length);
   })

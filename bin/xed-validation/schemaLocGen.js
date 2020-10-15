adc545cc6e2316f0fb45bba06ed513571cdaa415
jwen-adobe <jwen@adobe.com>
2020-10-14 17:15:05 -0700

    Add logging for invalid json file

--- bin/xed-validation/schemaLocGen.js
+++ bin/xed-validation/schemaLocGen.js
@@ -6,8 +6,12 @@ const glob = require("glob");
 glob("tempinput/**/*.schema.json", function(er, files) {
     let schemaLoc = {};
     files.forEach(function(file) {
-        let schema = JSON.parse(fs.readFileSync(file));
-        schemaLoc[schema["$id"]] = file.replace("tempinput/schemas/","xdm/").replace("tempinput/components/", "xdm/").replace("tempinput/extensions/", "xdm-extensions/");
+        try {
+            let schema = JSON.parse(fs.readFileSync(file));
+            schemaLoc[schema["$id"]] = file.replace("tempinput/schemas/","xdm/").replace("tempinput/components/", "xdm/").replace("tempinput/extensions/", "xdm-extensions/");
+        } catch(error) {
+            console.log(file.replace("tempinput/","") +": JSON " + error);
+        }
     });
     fs.writeFileSync('schemaLoc.json', JSON.stringify(schemaLoc,null,2));
 });

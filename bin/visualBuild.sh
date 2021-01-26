ddbdbf3044c050de3f974b8b94d9c0cbd6cffc3d
jwen-adobe <jwen@adobe.com>
2021-01-26 15:35:42 -0800

    Update visualBuild.sh

--- bin/visualBuild.sh
+++ bin/visualBuild.sh
@@ -1,6 +1,14 @@
 echo "triggering visual build"
 git clone https://github.com/adobe/xdmVisualization.git
 cd xdmVisualization
-cat dev.html
+echo "triggered" > triggeredBuild.log
+currentBranch=$(git status|grep "On branch"|cut -c11-100)
+echo $currentBranch
+git config --global user.email "jwen@adobe.com"
+git config --global user.name "jwen@adobe.com"
+git add .
+git commit -m "trigger a visualization build for master branch"
+git push --set-upstream origin $currentBranch
+
 cd ..
 rm -rf xdmVisualization

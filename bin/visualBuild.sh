43be443f1b29ac866b3cb6c9561b3807067e8606
jwen-adobe <jwen@adobe.com>
2021-01-26 15:47:08 -0800

    Update visualBuild.sh

--- bin/visualBuild.sh
+++ bin/visualBuild.sh
@@ -4,11 +4,8 @@ cd xdmVisualization
 echo "triggered" > triggeredBuild.log
 currentBranch=$(git status|grep "On branch"|cut -c11-100)
 echo $currentBranch
-echo "git add"
 git add .
-echo "git commit"
 git commit -m "trigger a visualization build for master branch"
-echo "git push"
-git push --set-upstream origin $currentBranch
+git push origin
 cd ..
 rm -rf xdmVisualization

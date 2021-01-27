b1c4e5aed4dcbd205863a4c4535a2ab2c2db8d8c
jwen-adobe <jwen@adobe.com>
2021-01-26 16:27:16 -0800

    trigger a visualization build for master branch

--- bin/visualBuild.sh
+++ bin/visualBuild.sh
@@ -1,5 +1,5 @@
 echo "triggering visual build"
-gh repo clone adobe/xdmVisualization
+git clone git@github.com:adobe/xdmVisualization.git
 cd xdmVisualization
 echo "triggered" > triggeredBuild.log
 currentBranch=$(git status|grep "On branch"|cut -c11-100)

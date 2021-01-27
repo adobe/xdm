5cf7a4a87edc736b96ce9ef0f8b141a9df4fe444
jwen-adobe <jwen@adobe.com>
2021-01-27 00:28:55 -0800

    Update visualBuild.sh

--- bin/visualBuild.sh
+++ bin/visualBuild.sh
@@ -1,7 +1,7 @@
 echo "triggering visual build..."
 git clone https://jwen-adobe:c831bac94b7bd842c7a7fe7c5ab115db4635d888@github.com/adobe/xdmVisualization.git
 cd xdmVisualization
-git remote set-url origin https://jwen-adobe:c831bac94b7bd842c7a7fe7c5ab115db4635d888@github.com/adobe/xdmVisualization.git
+#git remote set-url origin https://jwen-adobe:c831bac94b7bd842c7a7fe7c5ab115db4635d888@github.com/adobe/xdmVisualization.git
 echo "triggered" > triggeredBuild.log
 currentBranch=$(git status|grep "On branch"|cut -c11-100)
 echo $currentBranch

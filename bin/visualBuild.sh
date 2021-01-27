5a1f0ae5469fa56069b73f5c5f9e8279ddf96926
jwen-adobe <jwen@adobe.com>
2021-01-27 02:51:50 -0800

    test

--- bin/visualBuild.sh
+++ bin/visualBuild.sh
@@ -1,6 +1,9 @@
-echo "triggering visual build."
+echo "Visual build."
 git clone https://github.com/adobe/xdmVisualization.git
 cd xdmVisualization
+bundle init
+bundle install
+npm install -g grunt-cli
 npm install
 pwd
 ./scripts/xdmVisual4Git.sh master
